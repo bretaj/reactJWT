3ab6c4cc9dfa1861705e0f2f8ad6fce7ba5152ca
bret jensen <jensenbret13@gmail.com>
2025-03-23 19:37:27 -0700

    more authorization

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,6 +5,27 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ where: {username } });
+  if (!user) {
+    return res.status(404).json({ message: 'user not found' }); 
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: 'invalid password' });
+  }
+
+  const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET_KEY as string, {
+    expiresIn: '1h',
+  });
+
+  res.json ({ token });
+} catch (error) {
+  console.error(error);
+  res.status(500).json({ message: 'Internal server error' })
+}
 };
 
 const router = Router();
