7385d2547bc7ea279a47474a0ab7c7d0bd720f81
bret jensen <jensenbret13@gmail.com>
2025-03-31 11:28:43 -0700

    auth

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,14 +3,21 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    return token ? jwtDecode<JwtPayload>(token) : null;
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token;
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    if (!token) return true;
+    const decoded: JwtPayload = jwtDecode(token);
+    return decoded.exp ? decoded.exp * 1000 < Date.now() : true; 
   }
 
   getToken(): string {
